8ed42cc472569f382a3636aced0efd66eecb9163
Samvod21 <SamuelTeshome79@gmail.com>
2026-07-24 16:16:18 +0300

    Improve homepage with user profile bar and product detail

--- back/Routes/orderRoutes.js
+++ back/Routes/orderRoutes.js
@@ -8,8 +8,8 @@ const {
 } = require('../Controllers/orderController');
 const { protect, authorizeRoles } = require('../Middleware/authMidddleware');
 
-router.post('/', protect, authorizeRoles('Buyer'), createOrder);
-router.get('/mine', protect, authorizeRoles('Buyer'), getMyOrders);
+router.post('/', protect, authorizeRoles('Buyer', 'Supplier'), createOrder);
+router.get('/mine', protect, authorizeRoles('Buyer', 'Supplier'), getMyOrders);
 router.get('/seller', protect, authorizeRoles('Supplier'), getSellerOrders);
 router.put('/:id/status', protect, authorizeRoles('Supplier'), updateOrderStatus);
 
